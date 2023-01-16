2181816c0703fbc094256480181106eeeaa85895
jt-mullins <112453109+jt-mullins@users.noreply.github.com>
2023-01-16 09:24:09 -0600

    Delete app/public directory

--- app/public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$pdo = new PDO('mysql:dbname=MonsterMatch;host=mysql', 'jtmullins', 'secret', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-
-$query = $pdo->query('SHOW VARIABLES like "version"');
-
-$row = $query->fetch();
-
-echo 'MySQL version:' . $row['Value'];
