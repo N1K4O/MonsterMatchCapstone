6057418e5657be86040f5a90b2c192fc755e3733
jt-mullins <112453109+jt-mullins@users.noreply.github.com>
2023-01-16 19:20:19 -0600

    Removed unneeded files

--- Capstone2_monsterMatch/app/public/create_character.php
+++ Capstone2_monsterMatch/app/public/create_character.php
@@ -3,7 +3,7 @@
     $query = "INSERT INTO characters (user_id, character_name) VALUES (?, ?)";
     try {
         $pdo->prepare($query)->execute([$user["id"], $_POST["monster"]]);
-        echo '<script>window.location="/monster_created.html"</script>';
+        echo '<script>window.location.reload()</script>';
         exit();
     } catch (PDOException $e) {
         throw $e;
