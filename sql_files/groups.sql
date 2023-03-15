703ca7ea1ea1908a9b288401055d828b9897f092
jt-mullins <112453109+jt-mullins@users.noreply.github.com>
2023-03-15 18:08:27 -0500

    Add files via upload

---
+++ sql_files/groups.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `group_table` (
+  `idgroup` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `group_name` varchar(50) NOT NULL,
+  `group_leader` varchar(65) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  PRIMARY KEY (`idgroup`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
