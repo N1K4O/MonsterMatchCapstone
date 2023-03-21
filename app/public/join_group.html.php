9bb760503e39f886e0fbece69b7451d46a007063
jt-mullins <112453109+jt-mullins@users.noreply.github.com>
2023-03-20 21:48:55 -0500

    Several updates related to the game

--- app/public/join_group.html.php
+++ app/public/join_group.html.php
@@ -1,53 +1,46 @@
-<?php session_start() ?>
+<?php
+session_start();
+if (isset($_SESSION["user_id"])) {
+    $mysqli = require __DIR__ . "/database.php";
+    $sql = "SELECT * FROM user WHERE id = {$_SESSION["user_id"]}";
+    $result = $mysqli->query($sql);
+    $user = $result->fetch();
+}
+?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Join A Group</title>
+    <title>Create A Group</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Inter:300italic%2C400italic%2C600italic%2C700italic%2C800italic%2C400%2C300%2C600%2C700%2C800&amp;display=swap">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300italic%2C400italic%2C600italic%2C700italic%2C800italic%2C400%2C300%2C600%2C700%2C800&amp;display=swap">
 </head>
 
 <body>
-
-    <body style="/*background: url(&quot;design.jpg&quot;);*/background-position: 0 -60px;">
-        <nav id="mainNav"
-            class="navbar navbar-dark navbar-expand-md sticky-top border-secondary shadow navbar-shrink py-3">
-            <div class="container"><a class="navbar-brand d-flex align-items-center" href="/"><span>Monster Match</span></a><button class="navbar-toggler"
-                    data-bs-toggle="collapse" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle
-                        navigation</span><span class="navbar-toggler-icon"></span></button>
-                <div id="navcol-2" class="collapse navbar-collapse">
-                    <ul class="navbar-nav mx-auto">
-</ul> <a class="btn btn-outline-warning" role="button" href="logout.php">Log Out</a>
-                    
-                </div>
-            </div>
-        </nav>
-        <header class="bg-dark">
-            <div class="container pt-4 pt-xl-5">
-                <div class="row pt-5">
-                    <div class="col-md-8 col-xl-6 text-center text-md-start mx-auto">
-                        <div class="text-center">
-                            <h1 class="display-2 fw-bold text-center"><span style="color: rgb(4, 188, 44);">Join A Group</span></h1>
-                        </div>
+    <? include __DIR__ . '/navbar.html.php'; ?>
+    <section>
+        <div class="container pt-4 pt-xl-5">
+            <div class="row pt-5">
+                <div class="col-md-8 col-xl-6 text-center text-md-start mx-auto">
+                    <div class="text-center">
+                        <h3 class="display-3 fw-bold text-center"><span style="color: rgb(4, 188, 44);">Create A Group</span></h3>
+                        <br>
+                        <form action="join_group.php" method="post" id="join_group" novalidate>
+                            <div class="text-center">
+                                <input type="text" id="group_id" name="group_id" placeholder="Enter Group ID">
+<br><br>
+                                <button class="btn btn-outline-success shadow" role="button">Join</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
-            <br>
-
-
-                        <form action="join_group.php" method="post" id="join_group" novalidate>
-                            <div class="text-center">
-                                <!--<label for="name">Group Name</label>-->
-                                <input type="number" id="group_id" name="group_id" placeholder="Enter Group ID">
-
-                            <button>Join</button>
-                            </div>
-                        </form>
-
-    </body>
+        </div>
+    </section>
+</body>
 
 </html>
